23a107c4d5a201b036fe06ba42cc689fbe365a87
Nathan Classen <hayesclassen@gmail.com>
2020-03-10 23:29:56 -0500

    first commit: first attmpt at writing to db

--- chunker.js
+++ chunker.js
@@ -1,10 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
 const { spawnSync } = require("child_process");
 const { readFileSync, writeFileSync, unlinkSync, readdirSync, mkdirSync } = require("fs");
 const path = require("path");
 const AWS = require("aws-sdk");
 const outputBucketName = process.env.outputBucket;
-
 const s3 = new AWS.S3();
+
+AWS.config.update({ endpoint: "https://dynamodb.us-east-1.amazonaws.com" });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const jobsTable = "Jobs";
+const segmentsTable = "Segments";
 
 const sendVideoSegments = (s3path, bucket) => {
     readdirSync(s3path).forEach(fileName => {
@@ -20,6 +26,69 @@ const sendVideoSegments = (s3path, bucket) => {
         });
 
     });
+}
+
+const saveJobData = (id, tasks, file, fileFormat) => {
+    const id = id;
+    const filename = file;
+    const inputType = fileFormat;
+    const totalTasks = tasks.length;
+
+    const params = {
+        TableName: jobsTable,
+        Item: {
+            "id": id,
+            "type": "job",
+            "filename": filename,
+            "inputType": inputType,
+            //"outputType": outputType,
+            "totalTasks": totalTasks,
+            "finishedTasks": 0,
+            "status": "pending",
+            "createdAt": new Date,
+            "completedAt": null
+        }
+    };
+
+    console.log("Adding a new job...");
+    docClient.put(params, (err, data) => {
+        if (err) {
+            console.log("Unable to add job. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added job:", JSON.stringify(data, null, 2));
+        }
+    });
+}
+
+const saveSegmentData = (jobId, file, fileFormat) => {
+    const id = uuid4();
+    const filename = file;
+    const inputType = fileFormat;
+
+    const params = {
+        TableName: segmentsTable,
+        Item: {
+            "id": id,
+            "type": "segment",
+            "filename": filename,
+            "job_id": jobId,
+            "inputType": inputType,
+            //"outputType": outputType,
+            "status": "pending",
+            "createdAt": new Date,
+            "completedAt": null
+        }
+    };
+
+    console.log("Adding a new segment...");
+    docClient.put(params, (err, data) => {
+        if (err) {
+            console.log("Unable to add segment. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added segment:", JSON.stringify(data, null, 2));
+        }
+    });
+
 }
 
 module.exports.chunk = async (event, context) => {
@@ -42,11 +111,15 @@ module.exports.chunk = async (event, context) => {
             })
             .promise();
 
+        const jobId = uuid4();
+        const fileInfo = [...`${record.s3.object.key}`.match(/(.+)\.(.+)/)];
+        const filename = fileInfo[1];
+        const inputFormat = fileInfo[2];
+
         // write file to disk
         writeFileSync(`/tmp/${record.s3.object.key}`, s3Object.Body);
         writeFileSync("/tmp/chunks.ffcat", 'ffconcat version 1.0');
         mkdirSync("/tmp/videoSegments");
-
         // convert to mp4!
         spawnSync(
             "/opt/ffmpeg/ffmpeg",
@@ -72,10 +145,17 @@ module.exports.chunk = async (event, context) => {
             { stdio: "inherit" }
         );
 
+        const segmentListFile = readFileSync(`/tmp/chunks.ffcat`);
+        const segmentList = readdirSync('/tmp/videoSegments');
+
+        // WRITE TO DYNAMODB ##################################################
+        saveJobData(jobId, segmentList, filename, inputFormat);
+        saveSegmentData(jobId, filename, inputFormat);
+        //#####################################################################
+
         // send all segments to s3 for transcoding
         sendVideoSegments("/tmp/videoSegments", outputBucketName);
         // read segment manifest from disk
-        const segmentListFile = readFileSync(`/tmp/chunks.ffcat`);
 
         // delete the temp files
         unlinkSync(`/tmp/chunks.ffcat`);
